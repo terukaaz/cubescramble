68a515471b7c288cfd6712f92c2a26f444f1d14d
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-07-23 05:24:51 +0200

    (ig)

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pygame
 
 from scramblegenerator import clock
@@ -9,8 +11,14 @@ class Game:
         self.run = True
 
         self.font = pygame.font.Font("font.ttf", 25)
+        self.big_font = pygame.font.Font("font.ttf", 100)
+
         self.current_scramble = ""
         self.current_puzzle = "clock"
+
+        self.started = False
+        self.ready = 0
+        self.time_10ms = 0
 
 
     def refresh(self):
@@ -32,8 +40,28 @@ class Game:
                     self.run = False
                     return
 
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_r:
                     self.refresh()
+
+                if event.key == pygame.K_SPACE:
+                    if self.started:
+                        self.started = False # to stop the timer
+
+        if not self.started:
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                self.ready += 1
+            else:
+                if self.ready >= 20: # start the timer
+                    self.time_ms = 0
+                    self.started = True
+
+                self.ready = 0
+
+    def update_time(self):
+
+        if self.started:
+            self.time_10ms += 1
+
 
     def draw(self):
 
@@ -41,6 +69,26 @@ class Game:
         screen.fill((0, 0, 0))
 
         self.draw_text(self.current_scramble, (255, 255, 255), (10, 10))
+
+        ms_datetime = datetime.datetime.fromtimestamp(self.time_10ms / 100.0)
+
+        if self.time_10ms < 1000:
+            formatted_time = datetime.datetime.strftime(ms_datetime, "%S.%f")[:-4][1:]
+        elif 1000 < self.time_10ms < 6000:
+            formatted_time = datetime.datetime.strftime(ms_datetime, "%S.%f")[:-4]
+        elif 6000 < self.time_10ms < 10000:
+            formatted_time = datetime.datetime.strftime(ms_datetime, "%M:%S.%f")[:-4][1:]
+        elif self.time_10ms > 10000:
+            formatted_time = datetime.datetime.strftime(ms_datetime, "%M:%S.%f")[:-4]
+        else:
+            formatted_time = ""
+
+
+        print(ms_datetime)
+        print(formatted_time)
+        screen.blit(self.big_font.render(formatted_time, True, (255, 255, 255)), (screen.get_size()[0] / 2 - self.big_font.size(formatted_time)[0] / 2, screen.get_size()[1] / 2 - self.big_font.size(formatted_time)[1] / 2))
+
+
         pygame.display.update()
 
     def draw_text(self, text, color, coords):
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 import pygame
 import game
 
@@ -11,6 +14,29 @@ size = screen.get_size()
 
 game = game.Game(screen)
 run = True
+
+class PeriodicSleeper(threading.Thread):
+    def __init__(self, task_function, period):
+        super().__init__()
+        self.task_function = task_function
+        self.period = period
+        self.i = 0
+        self.t0 = time.time()
+        self.start()
+
+    def sleep(self):
+        self.i += 1
+        delta = self.t0 + self.period * self.i - time.time()
+        if delta > 0:
+            time.sleep(delta)
+
+    def run(self):
+        while 1:
+            self.task_function()
+            self.sleep()
+
+if run:
+    sleeper = PeriodicSleeper(game.update_time, 0.01)
 
 while run:
 
